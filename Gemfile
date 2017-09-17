bb7a7303926d0ff4c16566f24a7bf4f1fb66e1ab
jakegibs617 <jakegibs617@gmail.com>
2017-09-17 10:55:14 -0400

    console input returns all definitions

--- Gemfile
+++ Gemfile
@@ -2,4 +2,4 @@ source 'https://rubygems.org'
 
 gem 'nokogiri'
 gem 'httparty'
-gem 'pry'
+# gem 'pry'
--- code.rb
+++ code.rb
@@ -1,10 +1,27 @@
-require 'HTTParty'
-require 'Nokogiri'
+# require 'HTTParty'
+# require 'Nokogiri'
 require 'JSON'
-require 'Pry'
+require 'pry'
 require 'csv'
+require 'rest-client'
 
-page = HTTParty.get('http://www.urbandictionary.com/define.php?term=lite')
-parse_page = Nokogiri::HTML(page)
+puts "input word to read definition"
+word = gets
+url = "http://api.urbandictionary.com/v0/define?term=#{word}"
 
-Pry.start(binding)
+page = RestClient.get url, {accept: :json}
+
+parse_page = JSON.parse(page)
+definition = parse_page["list"][0]["definition"]
+
+
+def_array = []
+total = parse_page["list"].length
+
+for x in 0...total
+	def_array << parse_page["list"][x]["definition"]
+end
+
+puts def_array
+
+# Pry.start(binding)
